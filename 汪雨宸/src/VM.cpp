60a29a6b4ed8cf0bcada7a8568dc61befe131260
Goreign <2019210901@stu.cqupt.edu.cn>
2020-07-12 15:11:34 +0800

    汪雨宸

---
+++ 汪雨宸/src/VM.cpp
@@ -0,0 +1,15 @@
+﻿// VM.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <bitset>
+#include "VM.h"
+using namespace std;
+
+int main(){ 
+	VirtualMachine VM;
+	VM.Fetch(InputData());
+	VM.printf();
+	VM.Decode_Execute();
+	return 0;
+}
